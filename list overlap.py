8ad8f511f7f4a3ddcba6f08898188f8350cee698
destroyer91114 <yangxin.sz.ch@gmail.com>
2018-05-13 20:51:23 +1000

    my python projects

--- list overlap.py
+++ list overlap.py
@@ -2,6 +2,5 @@ a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89,200]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 200]
 
 for elem in a:
-    x = elem
-    if x in b:
+    if elem in b:
         print elem
